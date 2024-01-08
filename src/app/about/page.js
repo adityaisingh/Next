268f68578983e3b81160c58c450fcbf2820d750b
adityaisingh <ishu9918663060@gmail.com>
2024-01-08 12:45:30 +0530

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const about = () => {
+  return (
+    <div>
+      <h4>about use</h4>
+      <Link href="/"> Go to Home page</Link>
+    </div>
+  );
+};
+
+export default about;
